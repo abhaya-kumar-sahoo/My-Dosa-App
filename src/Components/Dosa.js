47edf5e4c0172db8a5fb5cd84fabf856de750447
Ridhichauhan <112615560+Ridhichauhan@users.noreply.github.com>
2023-07-10 11:18:58 +0530

    code updated

--- src/Components/Dosa.js
+++ src/Components/Dosa.js
@@ -1,40 +1,57 @@
-import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+import React,{useState} from "react";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import "./Dosa.css";
 import MenuItems from "./MenuItems";
-import '../utils/css/Gloablcss.css'
+import "../utils/css/Gloablcss.css";
 
 const Dosa = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const OpenSlider = () => {
+    setIsOpen(true);
+  };
+  const CloseSlider = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="">
       <div className="flex flex-row items-center justify-between p-2 creamcolor">
-        <div className="flex space-x-2  text-2xl ">
-          <h1 className="circle circle-text w-6 h-6 mb-2 sm:w-8 sm:h-8 sm:mb-4">
+        <div className="flex space-x-2 text-3xl xl:text-4xl">
+          <h1 className="circle circle-text  mb-4 xl:w-8 xl:h-8 xl:mb-6 mobile-hidden">
             x
           </h1>
-          <h1 className="text-orange-500 font-bold sm:text-xl">DOSA</h1>
-          <h1 className="text-green-600 font-bold sm:text-xl">HOUSE.</h1>
+          <div className="flex web-hidden">
+        <AiOutlineMenu color="red" size="0.7em" className="mt-2 ml-[-9px]" onClick={OpenSlider}/>
+      </div>
+          <h1 className="text-orange-500 font-bold xl:text-xl max-sm:text-[18px]">DOSA</h1>
+          <h1 className="text-green-600 font-bold xl:text-xl max-sm:text-[18px]">HOUSE.</h1>
         </div>
-        <div className="flex space-x-4 text-md text-black mt-[-20px]">
+        <div className="hidden xl:flex space-x-4 text-lg text-black mt-[-20px]">
           <h1 className="">Download Our App</h1>
           <h1 className="">Our Locations</h1>
           <h1 className="">Timings</h1>
           <h1 className="">Contact us</h1>
         </div>
       </div>
-  
-    <div className="flex justify-around ml-[-40px]  ">
-      <form className="form">
-        <div className="flex space-x-4 input-group">
-          <div className="relative">
-            <AiOutlineSearch className="absolute  mt-4 left-0 ml-2" color="gray" />
-            <input type="text" className="input " />
-            <label className="placeholder">Search Item</label>
+      <div className="flex justify-around ml-[-17vh]">
+        <form className="form max-sm:ml-14">
+          <div className="flex space-x-4 input-group">
+            <div className="relative">
+              <AiOutlineSearch
+                className="absolute max-sm:ml-16 ml-2 mt-4 left-0"
+                color="gray"
+              />
+              <input
+                type="text"
+                style={{ paddingLeft: 25 }}
+                className="input searchdown"
+              />
+              <label className="placeholder max-sm:ml-16">Search Item</label>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
-    <MenuItems/>
+        </form>
+      </div>
+      <MenuItems />
     </div>
   );
 };
--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -3,12 +3,12 @@ import sectionData from "./SectionData";
 
 const MenuItems = () => {
   return (
-    <div>
+    <div  className="scroll-container">
       {sectionData.map((item, index) => (
         <div key={index}>
-          <div className="flex abhaya justify-center ">
-            <div className="flex items-center space-x-2 w-[390px] ml-20 p-4 boxhover">
-              <img src={item.image} className="h-18 w-20 mt-2" alt="" />
+          <div className="flex justify-center">
+            <div className="flex items-center space-x-2 w-[390px] p-4 boxhover downbg">
+              <img src={item.image} className="h-18 w-20 mt-2 " alt="" />
               <div className="">
                 <h1 className="text-black text-left text-sm font-semibold">
                   {item.title}
